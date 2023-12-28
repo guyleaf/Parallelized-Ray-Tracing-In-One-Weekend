9c6d60efd79901ef644a76a961616c68b123e0ae
Leaf Ying <leaf.ying.work@gmail.com>
2023-12-28 17:37:22 +0000

    Set benchmark parameter step to 2

--- scripts/benchmark_omp.sh
+++ scripts/benchmark_omp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-hyperfine -w 3 -r 5 -P threads 1 10 --sort=command --shell=bash "OMP_NUM_THREADS={threads} taskset -c 0-9,20-29 ./build/openmp/OMPInOneWeekend"
+hyperfine -w 3 -r 5 -P threads 1 10 -D 2 --sort=command --shell=bash "OMP_NUM_THREADS={threads} taskset -c 0-9,20-29 ./build/openmp/OMPInOneWeekend"
