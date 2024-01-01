5b0e2436878ad7d5e5568bc7ab1f92585a67cffb
Leaf Ying <leaf.ying.work@gmail.com>
2024-01-01 09:35:26 +0000

    Update benchmark scripts

--- scripts/benchmark_omp.sh
+++ scripts/benchmark_omp.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-hyperfine -w 3 -r 5 -P threads 1 16 -D 2 --sort=command --shell=bash "OMP_NUM_THREADS={threads} taskset -c 0-9,20-29 ./build/openmp/OMPInOneWeekend"
-# hyperfine -w 3 -r 5 -P threads 1 16 -D 2 --sort=command --shell=bash "OMP_NUM_THREADS={threads} taskset -c 10-19,30-39 ./build/openmp/OMPInOneWeekend"
+CPUS="0-9,20-29"
+# CPUS="10-19,30-39"
+hyperfine -w 3 -r 5 -P threads 1 32 -D 1 --export-json=omp.json --sort=command --shell=bash "OMP_NUM_THREADS={threads} taskset -c ${CPUS} ./build/openmp/OMPInOneWeekend"
