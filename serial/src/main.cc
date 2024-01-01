9bac79eb95513744568daa139f1e9e352cf18379
Leaf Ying <leaf.ying.work@gmail.com>
2024-01-01 09:34:39 +0000

    Add MAP_SIZE macro to control number of objects

--- serial/src/main.cc
+++ serial/src/main.cc
@@ -28,6 +28,9 @@
 #ifndef MAX_DEPTH
 #define MAX_DEPTH 50
 #endif
+#ifndef MAP_SIZE
+#define MAP_SIZE 484
+#endif
 
 color ray_color(const ray& r, const hittable& world, int depth)
 {
@@ -52,14 +55,16 @@ color ray_color(const ray& r, const hittable& world, int depth)
 
 hittable_list random_scene()
 {
+    auto map_width = static_cast<int>(std::sqrt(MAP_SIZE));
+    auto half_map_width = map_width / 2;
     hittable_list world;
 
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++)
+    for (int a = -half_map_width; a < half_map_width; a++)
     {
-        for (int b = -11; b < 11; b++)
+        for (int b = -half_map_width; b < half_map_width; b++)
         {
             auto choose_mat = random_double();
             point3 center(a + 0.9 * random_double(), 0.2,
