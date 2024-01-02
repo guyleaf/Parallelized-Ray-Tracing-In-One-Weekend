325bf387cde9c24f69378da9602becd2cd34b754
Leaf Ying <leaf.ying.work@gmail.com>
2024-01-02 14:47:27 +0000

    Update benchmark scripts

--- scripts/benchmark_cuda.sh
+++ scripts/benchmark_cuda.sh
@@ -5,6 +5,6 @@ export CMAKE_BUILD_TYPE="Release"
 CPUS="10-19,30-39"
 
 CMD="taskset -c ${CPUS} ./build/cuda/CUDAInOneWeekend"
-SETUP='rm -r build && CUDA_BLOCK_SIZE={block_size} cmake -B build . && cmake --build build'
+SETUP='CUDAFLAGS="-DCUDA_BLOCK_SIZE={block_size}" cmake -B build --fresh . && cmake --build build'
 hyperfine -w 3 -r 5 -P block_size 1 32 -D 1 --export-json=cuda_block_size.json --sort=command --shell=bash --setup "${SETUP}" "${CMD}"
 # hyperfine -w 3 -r 5 --shell=bash "taskset -c 10-19,30-39 ./build/cuda/CUDAInOneWeekend"
